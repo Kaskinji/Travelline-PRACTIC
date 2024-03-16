22e4978006b3b8691f67385244f68c4b7f9afe4e
Никита <nk1kasatkin@yandex.ru>
2024-03-16 22:09:15 +0300

    Добавьте файлы проекта.

---
+++ Casino/Casino/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int balance = 10000;
+
+        while (balance > 0)
+        {
+            Console.WriteLine("Your balance: " + balance);
+            Console.Write("Enter your bet: ");
+            int bet = Convert.ToInt32(Console.ReadLine());
+
+            if (bet > balance)
+            {
+                Console.WriteLine("You cannot bet more than your balance. Try again.");
+                continue;
+            }
+
+            Random rnd = new Random();
+            int randomNum = rnd.Next(1, 21);
+
+            if (randomNum >= 18)
+            {
+                int multiplicator = rnd.Next(1, 17);
+                int win = bet * (1 + (multiplicator * (randomNum % 17)));
+                balance += win;
+                Console.WriteLine("You won " + win + " units!");
+            }
+            else
+            {
+                balance -= bet;
+                Console.WriteLine("You lost. Try again.");
+            }
+        }
+
+        Console.WriteLine("Game over. Your balance is 0. Thanks for playing!");
+    }
+}
---
+++ ConsoleApp1-travel/ConsoleApp1-travel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("ваше имя");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Введите количество заказа:");
+        int quantity = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите товар:");
+        string product = Console.ReadLine();
+
+        Console.WriteLine("Введите адрес доставки:");
+        string address = Console.ReadLine();
+
+        Console.WriteLine("\nПодтверждение введенных данных:");
+        Console.WriteLine($"Имя: {name}");
+        Console.WriteLine($"Количество заказа: {quantity}");
+        Console.WriteLine($"Товар: {product}");
+        Console.WriteLine($"Адрес доставки: {address}");
+    }
+}
