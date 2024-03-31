f1dae0233cb82d8cdc5d1af139f8764a29525309
Никита <nk1kasatkin@yandex.ru>
2024-03-31 19:41:57 +0300

    31 03 24

---
+++ Gladiators/Gladiators/IArmor.cs
@@ -0,0 +1,9 @@
+﻿namespace Fighters.models
+    public interface IFigther
+    {
+        public int MaxHealth { get }
+        public int CurrentHealth { get }
+        public int Power { get }
+        public IArmor Armor { get }
+        public int CalculateDamage { get }
+}
