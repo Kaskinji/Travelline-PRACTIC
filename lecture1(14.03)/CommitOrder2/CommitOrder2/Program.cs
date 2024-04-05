19b0b6ec5c587bb6745012c010f5f767df7d65fb
Никита <nk1kasatkin@yandex.ru>
2024-04-05 22:03:09 +0300

    правки

--- lecture1(14.03)/CommitOrder2/CommitOrder2/Program.cs
+++ lecture1(14.03)/CommitOrder2/CommitOrder2/Program.cs
@@ -10,25 +10,34 @@ class Program
         string? name = Console.ReadLine();
 
         Console.WriteLine("Введите количество вашего товара:");
-        int productQuantity = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Введите название вашего товара:");
-        string? product = Console.ReadLine();
-
-        Console.WriteLine("Введите адрес доставки:");
-        string? address = Console.ReadLine();
-        Console.WriteLine("\nПодтверждение введенных данных:");
-        Console.WriteLine($"Здравствуйте, {name}, вы заказали {productQuantity} {product} на адрес {address}, верно?");
-        Console.WriteLine("Это верно? -- да, нет");
-        string answer = Console.ReadLine();
-        switch (answer)
+        
+        try
         {
-            case "да":
-                Console.WriteLine($"{name}, ваш заказ: {product} в колличестве: {productQuantity} оформлен, ожидайте доставку по адресу: {address} к {todayDate}");
-                break;
-            default:
-                Console.WriteLine("Пожалуйста, перепроверьте свои данные");
-                break;
+            int? productQuantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите название вашего товара:");
+            string? product = Console.ReadLine();
+
+            Console.WriteLine("Введите адрес доставки:");
+             string? address = Console.ReadLine();
+            Console.WriteLine("\nПодтверждение введенных данных:");
+            Console.WriteLine($"Здравствуйте, {name}, вы заказали {productQuantity} {product} на адрес {address}, верно?");
+            Console.WriteLine("Это верно? -- да, нет");
+            string answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "да":
+                    Console.WriteLine($"{name}, ваш заказ: {product} в колличестве: {productQuantity} оформлен, ожидайте доставку по адресу: {address} к {todayDate}");
+                    break;
+                default:
+                    Console.WriteLine("Пожалуйста, перепроверьте свои данные");
+                    break;
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Некорректный ввод числа. Попробуйте ещё раз");
+            
         }
 
     }
