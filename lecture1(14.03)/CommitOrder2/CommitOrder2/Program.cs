c5eee928842dc55b0dc4d4d11fdd6d2144cfeba4
Никита <nk1kasatkin@yandex.ru>
2024-04-05 18:27:40 +0300

    05 04 24

--- lecture1(14.03)/CommitOrder2/CommitOrder2/Program.cs
+++ lecture1(14.03)/CommitOrder2/CommitOrder2/Program.cs
@@ -5,31 +5,31 @@ class Program
 
     static void Main()
     {
-        DateTime TodayDate = DateTime.Now;
-        Console.WriteLine("ваше имя");
+        DateTime todayDate = DateTime.Now;
+        Console.WriteLine("Введите ваше имя");
         string? name = Console.ReadLine();
 
-        Console.WriteLine("Введите количество заказа:");
-        int quantity = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите количество вашего товара:");
+        int productQuantity = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Введите название товара:");
+        Console.WriteLine("Введите название вашего товара:");
         string? product = Console.ReadLine();
 
         Console.WriteLine("Введите адрес доставки:");
         string? address = Console.ReadLine();
         Console.WriteLine("\nПодтверждение введенных данных:");
-        Console.WriteLine($"Здравствуйте, {name}, вы заказали {quantity} {product} на адрес {address}, верно?");
+        Console.WriteLine($"Здравствуйте, {name}, вы заказали {productQuantity} {product} на адрес {address}, верно?");
         Console.WriteLine("Это верно? -- да, нет");
         string answer = Console.ReadLine();
         switch (answer)
         {
             case "да":
-                Console.WriteLine($"{name}, ваш заказ: {product} в колличестве: {quantity} оформлен, ожидайте доставку по адресу: {address} к {TodayDate}");
+                Console.WriteLine($"{name}, ваш заказ: {product} в колличестве: {productQuantity} оформлен, ожидайте доставку по адресу: {address} к {todayDate}");
                 break;
             default:
                 Console.WriteLine("Пожалуйста, перепроверьте свои данные");
                 break;
         }
-        
+
     }
 }
